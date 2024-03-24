5dca9d801300c306cd2c67c7a2e27be4505b5139
Alberto Gomez Juan <alberto.gomez.j@bairesdev.com>
2024-03-24 14:22:58 +0100

    feat: :sparkles: entidad dentista

---
+++ internal/domain/dentista.go
@@ -0,0 +1,9 @@
+package domain
+
+// Dentista representa a un dentista
+type Dentista struct {
+	ID        int    `json:"id"`
+	Apellido  string `json:"apellido" binding:"required"`
+	Nombre    string `json:"nombre" binding:"required"`
+	Matricula string `json:"matricula" binding:"required"`
+}
